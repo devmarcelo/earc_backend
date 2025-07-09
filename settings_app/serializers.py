907d1cddbfd48d4528b82d343cbac525b1d0789d
devmarcelo <mpl.lopes82@gmail.com>
2025-07-09 10:04:40 -0300

    Atualização dos migrations em conformidade com novos padrões do projeto.

--- settings_app/serializers.py
+++ settings_app/serializers.py
@@ -1,14 +1,14 @@
 # settings_app/serializers.py
 
-# from rest_framework import serializers
-# from .models import Parameter, Integration
+from rest_framework import serializers
+from .models import Parameter, Integration
 
-# class ParameterSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Parameter
-#         fields = "__all__"
+class ParameterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Parameter
+        fields = "__all__"
 
-# class IntegrationSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Integration
-#         fields = "__all__"
+class IntegrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Integration
+        fields = "__all__"
--- settings_app/views.py
+++ settings_app/views.py
@@ -1,20 +1,20 @@
 # settings_app/views.py
 
-# from rest_framework import viewsets, permissions
-# from .models import Parameter, Integration
-# from .serializers import ParameterSerializer, IntegrationSerializer
-# from core.views import TenantAwareViewSet
+from rest_framework import viewsets, permissions
+from .models import Parameter, Integration
+from .serializers import ParameterSerializer, IntegrationSerializer
+from core.views import TenantAwareViewSet
 
-# class ParameterViewSet(TenantAwareViewSet):
-#     queryset = Parameter.objects.all()
-#     serializer_class = ParameterSerializer
-#     permission_classes = [permissions.IsAuthenticated]
-#     filterset_fields = ["key", "is_active"]
-#     search_fields = ["key", "value", "description"]
+class ParameterViewSet(TenantAwareViewSet):
+    queryset = Parameter.objects.all()
+    serializer_class = ParameterSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filterset_fields = ["key", "is_active"]
+    search_fields = ["key", "value", "description"]
 
-# class IntegrationViewSet(TenantAwareViewSet):
-#     queryset = Integration.objects.all()
-#     serializer_class = IntegrationSerializer
-#     permission_classes = [permissions.IsAuthenticated]
-#     filterset_fields = ["service", "is_active"]
-#     search_fields = ["name", "service", "description"]
+class IntegrationViewSet(TenantAwareViewSet):
+    queryset = Integration.objects.all()
+    serializer_class = IntegrationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filterset_fields = ["service", "is_active"]
+    search_fields = ["name", "service", "description"]
